0ff1dd2b15704170126aee01e971f28918ae52db
Nikita Hasert <nikitahasert@gmail.com>
2025-12-02 17:48:15 +0100

    Day 01: Add solutions

--- src/day_01.erl
+++ src/day_01.erl
@@ -1,8 +1,54 @@
 -module(day_01).
 -export([part1/1, part2/1]).
 
-part1(Input) ->
-    Input.
+-define(INITIAL_POS, 50).
+-define(MOD, 100).
 
-part2(Input) ->
-    Input.
+parse(Input) ->
+    Lines = string:split(Input, "\n", all),
+    TrimmedLines = lists:map(fun string:trim/1, Lines),
+    NonEmptyLines = lists:filter(fun(X) -> length(X) > 0 end, TrimmedLines),
+    lists:map(fun([Dir|Dist]) -> {rot, Dir, erlang:list_to_integer(Dist)} end, NonEmptyLines).
+
+next(CurrentAbs, {rot, $R, Dist}) -> CurrentAbs + Dist;
+next(CurrentAbs, {rot, $L, Dist}) -> CurrentAbs - Dist.
+
+rotate({dial, _, [], _} = R) -> R;
+rotate({dial, Curr, [Rot|T], States}) ->
+    Next = next(Curr, Rot),
+    rotate({dial, Next, T, States ++ [{Curr, Next}]}).
+
+part1({file, Input}) ->
+    {ok, Content} = file:read_file(Input),
+    part1({string, binary_to_list(Content)});
+part1({string, Input}) ->
+    State = {dial, ?INITIAL_POS, parse(Input), []},
+    {dial, _, _, DialStates} = rotate(State),
+    Positions = [?INITIAL_POS | lists:map(fun({_, Aft}) -> Aft end, DialStates)],
+    Zeroes = lists:filter(fun(AbsDialPos) -> (AbsDialPos rem 100) == 0 end, Positions),
+    length(Zeroes).
+
+floor_div(A, B) when A >= 0 -> A div B;
+floor_div(A, B) -> (A - B + 1) div B.
+
+count_passing_zero(R, []) -> R;
+count_passing_zero(R, [{Prev, Prev}|T]) -> count_passing_zero(R, T);
+count_passing_zero(R, [{Prev, Next}|T]) ->
+    Min = min(Prev, Next),
+    Max = max(Prev, Next),
+    % Count complete boundaries crossings
+    CrossedBoundary = floor_div(Max - 1, ?MOD) - floor_div(Min, ?MOD),
+    % Count if landing on a boundary, unless we started on one
+    OnBoundary = if
+        (Next rem ?MOD) == 0, (Prev rem ?MOD) /= 0 -> 1;
+        true -> 0
+    end,
+    count_passing_zero(R + CrossedBoundary + OnBoundary, T).
+
+part2({file, Input}) ->
+    {ok, Content} = file:read_file(Input),
+    part2({string, binary_to_list(Content)});
+part2({string, Input}) ->
+    State = {dial, ?INITIAL_POS, parse(Input), []},
+    {dial, _, _, DialStates} = rotate(State),
+    count_passing_zero(0, DialStates).
--- test/day_01_tests.erl
+++ test/day_01_tests.erl
@@ -1,8 +1,64 @@
 -module(day_01_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+part1_example_test() ->
+    ?assertEqual(3, day_01:part1({string, "L68
+    L30
+    R48
+    L5
+    R60
+    L55
+    L1
+    L99
+    R14
+    L82
+    "})).
+
 part1_test() ->
-    ?assertEqual(1, day_01:part1(1)).
+    ?assertEqual(1089, day_01:part1({file, "inputs/day_01.txt"})).
+
+part2_example_test() ->
+    ?assertEqual(6, day_01:part2({string, "L68
+    L30
+    R48
+    L5
+    R60
+    L55
+    L1
+    L99
+    R14
+    L82
+    "})).
+
+part2_1_test() ->
+    ?assertEqual(0, day_01:part2({string, "L0"})).
+
+part2_2_test() ->
+    ?assertEqual(1, day_01:part2({string, "L50"})).
+
+part2_3_test() ->
+    ?assertEqual(1, day_01:part2({string, "R50"})).
+
+part2_4_test() ->
+    ?assertEqual(1, day_01:part2({string, "L100"})).
+
+part2_5_test() ->
+    ?assertEqual(1, day_01:part2({string, "R100"})).
+
+part2_6_test() ->
+    ?assertEqual(2, day_01:part2({string, "L150"})).
+
+part2_7_test() ->
+    ?assertEqual(2, day_01:part2({string, "R150"})).
+
+part2_8_test() ->
+    ?assertEqual(7, day_01:part2({string, "L740"})).
+
+part2_9_test() ->
+    ?assertEqual(7, day_01:part2({string, "R730"})).
+
+part2_10_test() ->
+    ?assertEqual(8, day_01:part2({string, "L750"})).
 
 part2_test() ->
-    ?assertEqual(1, day_01:part2(1)).
+    ?assertEqual(6530, day_01:part2({file, "inputs/day_01.txt"})).

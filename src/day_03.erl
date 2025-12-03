9eaa584f51f170a2f1b33315ff81244134c27e8e
Nikita Hasert <nikitahasert@gmail.com>
2025-12-03 18:30:48 +0100

    Day 03: Add solutions

--- src/day_03.erl
+++ src/day_03.erl
@@ -1,8 +1,43 @@
 -module(day_03).
 -export([part1/1, part2/1]).
 
-part1(Input) ->
-    Input.
+parseBattery(Line) ->
+    lists:map(fun(B) -> list_to_integer([B]) end, Line).
 
-part2(Input) ->
-    Input.
+parse(Input) ->
+    Lines = string:split(Input, "\n", all),
+    TrimmedLines = lists:map(fun string:trim/1, Lines),
+    NonEmptyLines = lists:filter(fun(X) -> length(X) > 0 end, TrimmedLines),
+    lists:map(fun parseBattery/1, NonEmptyLines).
+
+maxVoltage([], Voltage, _) -> Voltage;
+maxVoltage(_, Voltage, 0) -> Voltage;
+maxVoltage(Batteries, Voltage, Remaining) when length(Batteries) == Remaining ->
+    RestVoltage = lists:foldl(fun(X, Sum) -> Sum * 10 + X end, 0 , Batteries),
+    TotalVoltage = Voltage * math:pow(10, length(Batteries)) + RestVoltage,
+    trunc(TotalVoltage);
+maxVoltage(Batteries, Voltage, Remaining) when length(Batteries) > Remaining ->
+    WindowSize = length(Batteries) - Remaining,
+    MaxVolt = lists:max(lists:sublist(Batteries, 1, WindowSize)),
+    [_|Rest] = lists:dropwhile(fun(B) -> B /= MaxVolt end, Batteries),
+    max(
+        % Slide window without choosing a battery
+        % Any other battery we would choose from the window
+        % would result in a lower voltage than the max
+        % so we can skip them
+        maxVoltage(Rest, Voltage, Remaining),
+        % Choose a battery and slide window
+        maxVoltage(Rest, Voltage * 10 + MaxVolt, Remaining - 1)
+    ).
+
+part({file, Input}, BatteryCount) ->
+    {ok, Content} = file:read_file(Input),
+    part({string, binary_to_list(Content)}, BatteryCount);
+part({string, Input}, BatteryCount) ->
+    Batteries = parse(Input),
+    lists:sum(
+        lists:map(fun(B) -> maxVoltage(B, 0, BatteryCount) end,
+            Batteries)).
+
+part1(Input) -> part(Input, 2).
+part2(Input) -> part(Input, 12).
--- test/day_03_tests.erl
+++ test/day_03_tests.erl
@@ -1,8 +1,20 @@
 -module(day_03_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+part1_example_test() ->
+    ?assertEqual(357, day_03:part1({string, "987654321111111
+    811111111111119
+    234234234234278
+    818181911112111"})).
+
 part1_test() ->
-    ?assertEqual(1, day_03:part1(1)).
+    ?assertEqual(17403, day_03:part1({file, "inputs/day_03.txt"})).
+
+part2_example_test() ->
+    ?assertEqual(3121910778619, day_03:part2({string, "987654321111111
+    811111111111119
+    234234234234278
+    818181911112111"})).
 
 part2_test() ->
-    ?assertEqual(1, day_03:part2(1)).
+    ?assertEqual(173416889848394, day_03:part2({file, "inputs/day_03.txt"})).

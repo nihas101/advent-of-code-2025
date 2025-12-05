223afbefcfced5478a8a8dceb480bae6db798e66
Nikita Hasert <nikitahasert@gmail.com>
2025-12-05 07:07:53 +0100

    Day05: Add solutions

--- src/day_05.erl
+++ src/day_05.erl
@@ -1,14 +1,64 @@
 -module(day_05).
 -export([part1/1, part2/1]).
 
+parseRange(Range) ->
+    [A, B] = string:split(Range, "-", all),
+    {range, list_to_integer(A), list_to_integer(B)}.
+
+splitLines(Lines) ->
+    SplitLines = string:split(Lines, "\n", all),
+    TrimmedLines = [string:trim(X) || X <- SplitLines],
+    lists:filter(fun(X) -> length(X) > 0 end, TrimmedLines).
+
+parseRanges(Ranges) ->
+    [parseRange(X) || X <- splitLines(Ranges)].
+
+parseIds(Ids) ->
+    [list_to_integer(X) || X <- splitLines(Ids)].
+
+parse(Input) ->
+    [FreshIdRanges, Ids] = string:split(Input, "\n\n", all),
+    {database, parseRanges(FreshIdRanges), parseIds(Ids)}.
+
+% This is not perfect, but good enough I guess.
+% Basing the iterations on the initial length means
+% we can 'overshoot' and do unnecessary iterations
+join_ranges([H|T] = FreshIdRanges) -> join_ranges(FreshIdRanges, join(H, T, []), length(FreshIdRanges)).
+join_ranges(FreshIdRanges, [H|T] = JoinedRanges, 0) when length(FreshIdRanges) /= length(JoinedRanges) ->
+    join_ranges(JoinedRanges, join(H, T, []), length(JoinedRanges));
+join_ranges(FreshIdRanges, JoinedRanges, 0) when length(FreshIdRanges) == length(JoinedRanges) ->
+    FreshIdRanges;
+join_ranges(_, [H|T] = JoinedRanges, X) ->
+    join_ranges(JoinedRanges, join(H, T, []), X-1).
+
+join(P, [], Output) -> Output ++ [P];
+join({range, A, B}, [{range, X, Y}|Ranges], Output) when A =< X, X =< B, A =< Y, Y =< B ->
+    join({range, A, B}, Ranges, Output);
+join({range, A, B}, [{range, X, Y}|Ranges], Output) when A =< X, X =< B, B =< Y ->
+    join({range, A, Y}, Ranges, Output);
+join({range, A, B}, [{range, X, Y}|Ranges], Output) when X =< A, A =< Y, Y =< B ->
+    join({range, X, B}, Ranges, Output);
+join(R1, [R2|Ranges], Output) ->
+    join(R1, Ranges, Output ++ [R2]).
+
+fresh([], _) -> false;
+fresh([{range, X, Y}|_], Id) when X =< Id, Id =< Y -> true;
+fresh([_|Ranges], Id) -> fresh(Ranges, Id).
+
+
 part1({file, Input}) ->
     {ok, Content} = file:read_file(Input),
     part1({string, binary_to_list(Content)});
 part1({string, Input}) ->
-    1.
+    {database, FreshIdRanges, Ids} = parse(Input),
+    JoinedRanges = join_ranges(FreshIdRanges),
+    Fresh = lists:filter(fun(X) -> fresh(JoinedRanges, X) end, Ids),
+    length(Fresh).
 
 part2({file, Input}) ->
     {ok, Content} = file:read_file(Input),
     part2({string, binary_to_list(Content)});
 part2({string, Input}) ->
-    1.
+    {database, FreshIdRanges, _} = parse(Input),
+    JoinedRanges = join_ranges(FreshIdRanges),
+    lists:sum([(Y - X) + 1 ||{range, X, Y} <- JoinedRanges]).
--- test/day_05_tests.erl
+++ test/day_05_tests.erl
@@ -2,13 +2,33 @@
 -include_lib("eunit/include/eunit.hrl").
 
 part1_example_test() ->
-    ?assertEqual(1, day_05:part1({string, ""})).
+    ?assertEqual(3, day_05:part1({string, "3-5
+    10-14
+    16-20
+    12-18
+
+    1
+    5
+    8
+    11
+    17
+    32"})).
 
 part1_test() ->
-    ?assertEqual(1, day_05:part1({file, "inputs/day_05.txt"})).
+    ?assertEqual(563, day_05:part1({file, "inputs/day_05.txt"})).
 
 part2_example_test() ->
-    ?assertEqual(1, day_05:part2({string, ""})).
+    ?assertEqual(14, day_05:part2({string, "3-5
+    10-14
+    16-20
+    12-18
+
+    1
+    5
+    8
+    11
+    17
+    32"})).
 
 part2_test() ->
-    ?assertEqual(1, day_05:part2({file, "inputs/day_05.txt"})).
+    ?assertEqual(338693411431456, day_05:part2({file, "inputs/day_05.txt"})).

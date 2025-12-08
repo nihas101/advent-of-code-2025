7c98ff3809cfb0168d5f4ded58e6891e8d148da8
Nikita Hasert <nikitahasert@gmail.com>
2025-12-08 17:31:47 +0100

    Day 08: Add solutions

--- src/day_08.erl
+++ src/day_08.erl
@@ -1,14 +1,62 @@
 -module(day_08).
--export([part1/1, part2/1]).
+-export([part1/1, part1/2, part2/1]).
+
+parse_line(L) ->
+    [X, Y, Z] = string:split(L, ",", all),
+    {pos, list_to_integer(X), list_to_integer(Y), list_to_integer(Z)}.
+
+parse(Input) ->
+    Lines = string:split(Input, "\n", all),
+    TrimmedLines = lists:map(fun string:trim/1, Lines),
+    NonEmptyLines = lists:filter(fun(X) -> length(X) > 0 end, TrimmedLines),
+    [parse_line(L) || L <- NonEmptyLines].
+
+euclidian_distance_squared({pos, PX, PY, PZ}, {pos, QX, QY, QZ}) ->
+    DX = (PX - QX),
+    DY = (PY - QY),
+    DZ = (PZ - QZ),
+    (DX * DX) + (DY * DY) + (DZ * DZ).
+
+distance_map([P|T]) -> distance_map(P, T, T, #{}).
+distance_map(_, [], [], DistanceMap) -> DistanceMap;
+distance_map(_, [], [P|T], DistanceMap) -> distance_map(P, T, T, DistanceMap);
+distance_map(P, [Q|T], S, DistanceMap) ->
+    Dist = euclidian_distance_squared(P,Q),
+    NewDistanceMap = DistanceMap#{ {P,Q} => Dist },
+    distance_map(P, T, S, NewDistanceMap).
+
+connect(_, Circuits, 0) -> Circuits;
+connect([{{P, Q}, _} | T], Circuits, ConnectionsCount) ->
+    {ConnectingCircuits, DisconnectedCircuits} = lists:partition(fun(C) -> sets:is_element(P, C) or sets:is_element(Q, C) end, Circuits),
+    NewCircuit = sets:union(ConnectingCircuits),
+    connect(T, [NewCircuit] ++ DisconnectedCircuits, ConnectionsCount - 1).
 
 part1({file, Input}) ->
     {ok, Content} = file:read_file(Input),
-    part1({string, binary_to_list(Content)});
-part1({string, Input}) ->
-    1.
+    part1({string, binary_to_list(Content)}, 1000).
+part1({string, Input}, ConnectionsCount) ->
+    JunctionBoxes = parse(Input),
+    DistanceMap = distance_map(JunctionBoxes),
+    SortedJunctionBoxes = lists:sort(fun({_, DistA},{_, DistB}) -> DistA < DistB end, maps:to_list(DistanceMap)),
+    InitialConnections = lists:map(fun(JB) -> sets:from_list([JB]) end, JunctionBoxes),
+    ConnectedJunctionBoxes = connect(SortedJunctionBoxes, InitialConnections, ConnectionsCount),
+    CircuitSizes = lists:map(fun(CJB) -> length(sets:to_list(CJB)) end, ConnectedJunctionBoxes),
+    [A, B, C | _] = lists:sort(fun(A, B) -> A > B end, CircuitSizes),
+    A * B * C.
+
+connect_all(_, Circuits, Previous) when length(Circuits) == 1 -> Previous;
+connect_all([{{P, Q}, _} | T], Circuits, _) ->
+    {ConnectingCircuits, DisconnectedCircuits} = lists:partition(fun(C) -> sets:is_element(P, C) or sets:is_element(Q, C) end, Circuits),
+    NewCircuit = sets:union(ConnectingCircuits),
+    connect_all(T, [NewCircuit] ++ DisconnectedCircuits, {P, Q}).
 
 part2({file, Input}) ->
     {ok, Content} = file:read_file(Input),
     part2({string, binary_to_list(Content)});
 part2({string, Input}) ->
-    1.
+    JunctionBoxes = parse(Input),
+    DistanceMap = distance_map(JunctionBoxes),
+    SortedJunctionBoxes = lists:sort(fun({_, DistA},{_, DistB}) -> DistA < DistB end, maps:to_list(DistanceMap)),
+    InitialConnections = lists:map(fun(JB) -> sets:from_list([JB]) end, JunctionBoxes),
+    {{pos, X1, _, _}, {pos, X2, _, _}} = connect_all(SortedJunctionBoxes, InitialConnections, {}),
+    X1 * X2.
--- test/day_08_tests.erl
+++ test/day_08_tests.erl
@@ -2,13 +2,51 @@
 -include_lib("eunit/include/eunit.hrl").
 
 part1_example_test() ->
-    ?assertEqual(1, day_08:part1({string, ""})).
+    ?assertEqual(40, day_08:part1({string, "162,817,812
+    57,618,57
+    906,360,560
+    592,479,940
+    352,342,300
+    466,668,158
+    542,29,236
+    431,825,988
+    739,650,466
+    52,470,668
+    216,146,977
+    819,987,18
+    117,168,530
+    805,96,715
+    346,949,466
+    970,615,88
+    941,993,340
+    862,61,35
+    984,92,344
+    425,690,689"}, 10)).
 
 part1_test() ->
-    ?assertEqual(1, day_08:part1({file, "inputs/day_08.txt"})).
+    ?assertEqual(46398, day_08:part1({file, "inputs/day_08.txt"})).
 
 part2_example_test() ->
-    ?assertEqual(1, day_08:part2({string, ""})).
+    ?assertEqual(25272, day_08:part2({string, "162,817,812
+    57,618,57
+    906,360,560
+    592,479,940
+    352,342,300
+    466,668,158
+    542,29,236
+    431,825,988
+    739,650,466
+    52,470,668
+    216,146,977
+    819,987,18
+    117,168,530
+    805,96,715
+    346,949,466
+    970,615,88
+    941,993,340
+    862,61,35
+    984,92,344
+    425,690,689"})).
 
 part2_test() ->
-    ?assertEqual(1, day_08:part2({file, "inputs/day_08.txt"})).
+    ?assertEqual(8141888143, day_08:part2({file, "inputs/day_08.txt"})).
